0ad18e9fc0db9b679f281aec4198894816f11ff9
Miles Breslin <finalbrez@gmail.com>
2018-05-17 20:22:44 -0700

    Tested for accuracy & updated for timings

--- TrafficCycles.m
+++ TrafficCycles.m
@@ -10,7 +10,7 @@ CYCLEMAP =	importdata(f_CYCLEMAP);
 
 %% Load Labjack
 
-ljud_LoadDrivers
+ljud_LoadDriver
 ljud_Constants
 
 [Error ljHandle] = ljud_OpenLabJack(LJ_dtU3,LJ_ctUSB,'1',1);
@@ -25,11 +25,13 @@ DONE = false;		%%Allow Safe escape if implemented
 
 while ~DONE
 	for n = 1:length(CYCLEMAP(:,1))
-		SetCycle(CYCLEMAP(n,:),PINMAP);							%%Selected On
-		sleep(CYCLEMAP(end,n));
-		SetCycle(CYCLEMAP(n,:) *.5,PINMAP);						%%Selected "Yellow" simulated as 50% intensity
-		sleep(2);
-		SetCycle(zeros(1,length(CYCLEMAP(1,:))), PINMAP);		%%All Off
-		sleep(3);
+		SetCycle(ljHandle,CYCLEMAP(n,:),PINMAP, LJ_ioPUT_DIGITAL_BIT);							%%Selected On
+        fprintf("SET Cycle %d\n",n);
+		pause(CYCLEMAP(n,end));
+		%%SetCycle(CYCLEMAP(n,:) *.5,PINMAP);						%%Selected "Yellow" simulated as 50% intensity
+		%%pause(2);
+		SetCycle(ljHandle,zeros(1,length(CYCLEMAP(1,:))), PINMAP, LJ_ioPUT_DIGITAL_BIT);		%%All Off
+        fprintf("CLEAR Cycle %d\n",n);
+		pause(3);
 	end
 end
