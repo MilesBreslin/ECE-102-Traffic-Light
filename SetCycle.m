b89ec5ec8be6c32ec44c8220698f931c1a94f851
Miles Breslin <mbreslin@ds.cecs.pdx.edu>
2018-06-04 18:47:49 -0700

    Fixed some : problems and tested on real hardware

--- SetCycle.m
+++ SetCycle.m
@@ -1,7 +1,7 @@
 function r = SetCycle(ljHandle, CYCLEMAP, PINMAP, LJ_ioPUT_DIGITAL_BIT)
 	for n = 1:length(CYCLEMAP)
-        for p = (1:length(PINMAP(:,1)))-1       %%Cycle through all of the colors
-            ljud_ePut(ljHandle, LJ_ioPUT_DIGITAL_BIT,PINMAP(n,p), CYCLEMAP(n) == p ,0);     %%If the number in the cycle map is the correct pin, on, else, off
+        for p = (1:length(PINMAP(1,:)))-1       %%Cycle through all of the colors
+            ljud_ePut(ljHandle, LJ_ioPUT_DIGITAL_BIT,PINMAP(n,p+1), CYCLEMAP(n)==p ,0);     %%If the number in the cycle map is the correct pin, on, else, off
         end
     end
 end
